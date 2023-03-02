288c5ab6e0207e3c3881d0c98dde698c68dfb2d1
PavanKalyan <pavankalyank4496@gmail.com>
2023-03-02 04:22:17 +0000

    Solution

---
+++ app.js
@@ -0,0 +1,12 @@
+let express = require("express");
+let app = express();
+
+app.get("/", (request, response) => {
+  response.send("Home Page");
+});
+
+app.get("/about", (request, response) => {
+  response.send("About Page");
+});
+
+module.exports = app;
